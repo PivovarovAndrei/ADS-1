c190762d72ce9addaedafd69364f060916367096
PivovarovAndrei <160382197+PivovarovAndrei@users.noreply.github.com>
2024-04-15 19:26:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,6 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
   for (uint64_t i = 2; i*i <= value; ++i) {
   if (value % i == 0)
     return false;
@@ -13,10 +12,9 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
   int c = 0;
-int num = 1;
-while (c < n) {
+  int num = 1;
+  while (c < n) {
   num++;
   if (checkPrime(num)) {
     c++;
@@ -26,16 +24,14 @@ return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
   int num = ++value;
-while (!checkPrime(num)) {
+  while (!checkPrime(num)) {
   num++;
   }
   return num;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
   for(int num = 2; num < hbound; num++) {
   if (checkPrime(num))
       sum += num;
